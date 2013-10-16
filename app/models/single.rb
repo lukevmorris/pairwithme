bfb1946a7e21bb9dfa6deb66b39354b265903cc5
Luke Morris <luke@imfuna.com>
2013-10-16 16:31:03 -0700

    Basic tests for a Single.

--- app/models/single.rb
+++ app/models/single.rb
@@ -1,3 +1,16 @@
 class Single
-  
+  attr_accessor :seeking
+
+  def initialize(options = {})
+    @seeking = options.fetch(:seeking, false)
+  end
+
+  def toggle_seeking_status
+    seeking ? stop_seeking : start_seeking
+  end
+
+private
+  def start_seeking; self.seeking = true end
+  def stop_seeking; self.seeking = false end
 end
+
---
+++ spec/models/single_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Single do
+  it 'is not seeking a pair by default' do
+    single = Single.new
+    expect(single.seeking).to eq false
+  end
+end
+
+describe Single, '#toggle_seeking_status' do
+  it 'flips an individual\'s seeking status from true to false' do
+    single = Single.new(seeking: true)
+    single.toggle_seeking_status
+    expect(single.seeking).to be_false
+  end
+
+  it 'flips an individual\'s seeking status from false to true' do
+    single = Single.new(seeking: false)
+    single.toggle_seeking_status
+    expect(single.seeking).to be_true
+  end
+end

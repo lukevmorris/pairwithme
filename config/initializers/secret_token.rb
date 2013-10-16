fdcb7303c4001b8ecd211fb05779e28196a97219
Luke Morris <luke@imfuna.com>
2013-10-16 15:35:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pairwithme::Application.config.secret_key_base = 'c1e22e445360ced0b79765b454bcefd26abc57c7e69eea9ede470201b19e7d2af0448617dde1c9c5091cbc4998aec5a865b6e2584e27630a189029b59bc5a8c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pairwithme::Application.config.session_store :cookie_store, key: '_pairwithme_session'
